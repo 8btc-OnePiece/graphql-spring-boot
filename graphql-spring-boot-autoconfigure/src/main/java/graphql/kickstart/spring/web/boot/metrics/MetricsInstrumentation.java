59d3420fe70b5d6aa5ecb51d049aabd30439e798
guodage <guodage003@gmail.com>
2023-07-19 11:11:30 +0800

    metrics tag-classification use the exception's cause instead of CompletionException

--- graphql-spring-boot-autoconfigure/src/main/java/graphql/kickstart/spring/web/boot/metrics/MetricsInstrumentation.java
+++ graphql-spring-boot-autoconfigure/src/main/java/graphql/kickstart/spring/web/boot/metrics/MetricsInstrumentation.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.math.NumberUtils;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -108,7 +109,13 @@ public class MetricsInstrumentation extends TracingInstrumentation {
                 if (error.getExtensions() != null && error.getExtensions().containsKey("classification")) {
                     classification = error.getExtensions().get("classification").toString();
                 }else if (error instanceof ExceptionWhileDataFetching){
-                    classification = ((ExceptionWhileDataFetching) error).getException().getClass().getName();
+                    //the metrics only focus this category
+                    ExceptionWhileDataFetching ewdf = (ExceptionWhileDataFetching) error;
+                    if (ewdf.getException() instanceof CompletionException) {
+                        classification = ewdf.getException().getCause().getClass().getName();
+                    }else {
+                        classification = ewdf.getException().getClass().getName();
+                    }
                 }
 
                 //special requirement: some code is null, or some error is not real error.
